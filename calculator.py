557f5730a8679db1f305a92fdcccce853689bf5d
HelenEdwards <hgedwards87@gmail.com>
2024-06-05 17:33:17 -0700

    All done

--- calculator.py
+++ calculator.py
@@ -5,3 +5,25 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 
 # Replace this with your code
+
+while True:
+# repeat forever:
+    user_input=input("Type your equation here.> ")
+#     read input
+    tokenize_input=user_input.split(" ")
+    command=tokenize_input[0]
+#    tokenize input
+    if command == "q":
+        break
+#         if the first token is "q":
+#             quit
+    elif command == "add":
+        add(tokenize_input[1], tokenize_input[2])
+
+#         else:
+#             (decide which math function to call based on first token)
+#             if the first token is 'pow':
+#                   call the power function with the other two tokens
+
+#             (...etc.)
+# 
