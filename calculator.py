949e4277e753713d607a71968037b0288140132e
HelenEdwards <hgedwards87@gmail.com>
2024-06-05 17:41:51 -0700

    For real done

--- calculator.py
+++ calculator.py
@@ -1,29 +1,46 @@
-"""CLI application for a prefix-notation calculator."""
-
-from arithmetic import (add, subtract, multiply, divide, square, cube,
-                        power, mod, )
-
-
-# Replace this with your code
-
-while True:
+# """CLI application for a prefix-notation calculator."""
+# """
 # repeat forever:
-    user_input=input("Type your equation here.> ")
 #     read input
-    tokenize_input=user_input.split(" ")
-    command=tokenize_input[0]
-#    tokenize input
-    if command == "q":
-        break
+#     tokenize input
 #         if the first token is "q":
 #             quit
-    elif command == "add":
-        add(tokenize_input[1], tokenize_input[2])
-
 #         else:
 #             (decide which math function to call based on first token)
 #             if the first token is 'pow':
 #                   call the power function with the other two tokens
 
 #             (...etc.)
-# 
+          
+# """
+
+
+from arithmetic import (add, subtract, multiply, divide, square, cube,
+                        power, mod, )
+#
+while True:
+#
+    user_input=input("Type your equation here.> ")
+#
+    tokenize_input=user_input.split(" ")
+    command=tokenize_input[0]
+#
+    if command == "q":
+        break
+    elif command == "add":
+        add(int(tokenize_input[1]), int(tokenize_input[2]))
+    elif command == "subtract":
+        subtract(int(tokenize_input[1]), int(tokenize_input[2]))
+    elif command == "multiply":
+        multiply(int(tokenize_input[1]), int(tokenize_input[2]))
+    elif command == "divide":
+        divide(int(tokenize_input[1]), int(tokenize_input[2]))
+    elif command == "square": 
+        square(int(tokenize_input[1]))
+    elif command == "cube":
+        cube(int(tokenize_input[1]))
+    elif command == "power":
+        power(int(tokenize_input[1]), int(tokenize_input[2]))
+    elif command == "mod":
+        mod(int(tokenize_input[1]), int(tokenize_input[2]))
+        
